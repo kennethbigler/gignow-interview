3c1e9d3b93f213afbabb22a7622801f461b84f2c
ken.w.bigler <Ken.W.Bigler@ey.com>
2018-12-04 14:14:51 -0800

    adding more requirements and a onclick handler

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  static propTypes = {
+    onClick: propTypes.func.isRequired,
+  };
+
   render() {
     return (
       <div className="App">
@@ -30,7 +35,9 @@ class App extends Component {
 2. They should be ordered with the most recent event on top
 3. Viewing the same job req 2x should be ignored
 4. Viewing a job reqs on different days should be separated into 2 events
-5. Nice to have - can we show the date only 1x? see design
+5. On click of req should call the callback function passed in
+6. Nice to have - can we show the date only 1x? see design
+7. If you have time - move activity logs to state, and add a "Delete" functionality
 */
 
 export default App;
